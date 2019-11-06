62393baa9676725b0587685d3a399cb78dfcd4eb
umncsk <umnc.sk@gmail.com>
2019-11-06 10:21:39 +0900

    update serializers.py

--- app_scheduler/serializers.py
+++ app_scheduler/serializers.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        fields = (
-            'id',
-            'user_name',
-            'organization',
-            'user_schedule',
-        )
         model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
