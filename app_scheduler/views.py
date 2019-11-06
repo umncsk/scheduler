c485eea1caaa1f5c7c3081e6ae27705edcba9655
umncsk <umnc.sk@gmail.com>
2019-11-06 10:44:31 +0900

    restful django api initialization

--- app_scheduler/views.py
+++ app_scheduler/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# Django Rest Framework views
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from app_scheduler.serializers import UserSerializer, GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- scheduler/urls.py
+++ scheduler/urls.py
@@ -18,7 +18,13 @@ from django.urls import include, path
 from rest_framework import routers
 from app_scheduler import views
 
+# Django Rest Framework routers
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    # path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
