e9b596003766ac801c6d2e7522ec5156d1f532f3
umncsk <umnc.sk@gmail.com>
2019-11-18 11:00:09 +0900

    delete debug lines

--- app_scheduler/serializers.py
+++ app_scheduler/serializers.py
@@ -8,7 +8,7 @@ from app_scheduler.scraping import get_user_schedule
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('user_name', 'user_pswd', 'student_id', 'student_pswd', 'user_schedule')
+        fields = ('user_name', 'user_pswd', 'student_id', 'student_pswd')
 
     def create(self, validated_data):
         user_pswd = validated_data.get('user_pswd')
