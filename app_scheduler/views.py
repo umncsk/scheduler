82053ff549a9d6fc529f8ab0df99bdff177cdce3
umncsk <umnc.sk@gmail.com>
2019-11-21 20:08:36 +0900

    fix importation

--- app_scheduler/views.py
+++ app_scheduler/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 # Django Rest Framework views
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import viewsets, status, generics
-from rest_framework.parsers import JSONParser
+# from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from app_scheduler.models import User
