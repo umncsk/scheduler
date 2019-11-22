5659fe78c25ae7649ee7f9cd3011d0c6d061c671
umncsk <umnc.sk@gmail.com>
2019-11-23 08:04:58 +0900

    update urls

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -23,7 +23,6 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', include('app_scheduler.urls')),
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
