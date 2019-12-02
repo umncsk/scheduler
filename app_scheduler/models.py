1ba23cfc7738a9d4aae60130b5334eff94227a6e
umncsk <umnc.sk@gmail.com>
2019-12-02 15:24:31 +0900

    不要なテーブルの削除

--- app_scheduler/models.py
+++ app_scheduler/models.py
@@ -3,18 +3,17 @@ from django.db import models
 class User(models.Model):
     # Referencing a foreign key from the Organization class
     organization  = models.ForeignKey("Organization", to_field='team_id', on_delete=models.SET_NULL, null=True)
+    student_id    = models.CharField(max_length=14, null=False, primary_key=True)
     user_name     = models.CharField(max_length=256, null=False)
-    user_pswd     = models.CharField(max_length=256, null=False)
-    student_id    = models.CharField(max_length=20, null=False)
-    student_pswd  = models.CharField(max_length=256, null=False)
-    user_schedule = models.CharField(max_length=256)
+    passwd        = models.CharField(max_length=256, null=False)
+    schedule      = models.CharField(max_length=256, null=True)
 
     def __str__(self):
         return self.student_id
 
 class Organization(models.Model):
-    team_id = models.CharField(max_length=256, primary_key=True)
-    team_schedule = models.CharField(max_length=256)
+    team_id       = models.CharField(max_length=256, primary_key=True)
+    schedule      = models.CharField(max_length=256, null=True)
 
     def __str__(self):
         return self.team_id
