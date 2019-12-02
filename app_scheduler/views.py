0de01574237dde7686bfb79b0eb4d973f9f12322
umncsk <umnc.sk@gmail.com>
2019-12-03 01:11:19 +0900

    使用していないモジュールを除外

--- app_scheduler/views.py
+++ app_scheduler/views.py
@@ -1,23 +1,16 @@
 # from django.shortcuts import render
 # from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 # Django Rest Framework views
 from django.contrib.auth.models import User
-from rest_framework import viewsets, status, generics
+from rest_framework import viewsets
 # from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from app_scheduler.models import User, Organization
 from app_scheduler.serializers import UserSerializer, OrganizationSerializer
 
-"""
-class DetailSchedule(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-"""
 
 class UserViewSet(viewsets.ModelViewSet):
     # API endpoint that allows users to be viewed or edited.
