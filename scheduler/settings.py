17e0c593f6711deb1de4bcfa02a37b67fd919d38
umncsk <umnc.sk@gmail.com>
2019-12-09 08:17:30 +0900

    regenerate SECRET_KEY

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,6 +25,7 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+SECRET_KEY=SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
