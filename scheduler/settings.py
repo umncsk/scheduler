060b2c8fcc97de5cc2a23fa9435f5ea140fbf55c
umncsk <umnc.sk@gmail.com>
2019-12-09 08:26:55 +0900

    前のだとビルド落ちするので

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -21,11 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-SECRET_KEY=SECRET_KEY
+# make localsettings.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,3 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
