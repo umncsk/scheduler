bb4d41ca9d2a5f21244a6624c4dab073dbb79e94
umncsk <umnc.sk@gmail.com>
2019-12-09 08:09:46 +0900

    regenerate SECRET_KEY

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h)2k_%k8@uv5n0k2ybw5+lbasrzodusitkre0_!gi73m4+y^nm'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
